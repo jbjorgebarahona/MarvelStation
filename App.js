6b4bb1812841ec74973c1ba6cafef9e53e20c232
jbjorgebarahona <jorge_129803@hotmail.com>
2021-08-03 14:45:54 -0600

    parte 7

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+
+import React, { useEffect,useState } from 'react';
 import './src/App.css'
 import {  Text, View } from 'react-native';
 import Navbar from './src/components/Navbar';
@@ -10,12 +10,37 @@ import CheckoutPage from './src/components/CheckoutPage';
 import CheckoutCard from './src/components/CheckoutCard';
 import Typography from '@material-ui/core/Typography';
 import { Switch,BrowserRouter as Router, Route } from 'react-router-dom';
-import { StateProvider } from './src/StateProvider';
 import reducer, { initialState } from "./src/reducer";
 import SignIn from './src/Singin';
 import SignUp from './src/Singup';
- 
+import { auth } from './src/firebase/firebase';
+import { StateProvider } from './src/StateProvider';
+import { actionTypes } from './src/reducer';
+import { useStateValue } from './src/StateProvider';
+import firebase from 'firebase';
+
 export default function App() {
+  const dispatch= useStateValue();
+  //const [dispatch,{user}] = useStateValue();
+  const [user, setUser] = useState();
+  
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
+      if (authUser) {
+        setUser({
+          type: actionTypes.SET_USER,
+          user: authUser,
+        });
+      } else {
+        setUser({
+          type: actionTypes.SET_USER,
+          user: null,
+        });
+      }
+    });
+  }, []);
+
  return (
    <StateProvider initialState={initialState} reducer={reducer}>
      <Router>
